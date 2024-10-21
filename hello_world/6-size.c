d1a30dd42a4ed2c1ad227d0bc1d1538bd9679672
root <root@DESKTOP-46AJ964>
2024-10-21 20:29:33 +0200

    ajout fichier 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	sizeof(char);
+	sizeof(int);
+	sizeof(long int);
+	sizeof(long long int);
+	sizeof(float);
+	return (0);
+}
