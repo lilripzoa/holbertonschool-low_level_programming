61ff46bb81557e37dc9bbde85001dc2a1bbe641a
root <root@DESKTOP-46AJ964>
2024-10-23 15:34:33 +0200

    ajout du fichier 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int y;
+
+	for (y = '0'; y <= '9'; y++)
+		putchar(y);
+	putchar('\n');
+
+	return (0);
+}
