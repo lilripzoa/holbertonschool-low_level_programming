e8f473a7d165c444b09d29007e5ea9e78ab73598
root <root@DESKTOP-46AJ964>
2024-10-24 16:19:21 +0200

    ajout des fichiers 1-main.c 1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+#include "main.c"
+
+/**
+ *  * main - check the code
+ *   *
+ *    * Return: Always 0.
+ *     */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	} 
+      _putchar('\n');	
+
+	    return (0);
+}
