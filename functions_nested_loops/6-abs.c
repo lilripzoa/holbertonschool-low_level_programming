c45566118e3eb9505539447d771806b7983f9c28
root <root@DESKTOP-46AJ964>
2024-10-25 17:00:22 +0200

    ajout des fichiers 6 update du fichier main.h

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - calcule la valeur absolue d'un entier.
+ * @c: l'entier dont on veut la valeur absolue.
+ * 
+ * Return: la valeur absolue de n.
+ */
+
+int _abs(int)
+{
+	int c;
+	int b = _abs(c)
+	
+	if (b >= '0')
+	{
+		putchar(c);
+		return (c);
+	}
+	return (0);
+
+
+}
