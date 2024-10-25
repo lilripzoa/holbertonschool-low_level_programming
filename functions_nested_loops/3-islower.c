d9e1d1ec480ef246b820dc6a2da333eb1ec55605
root <root@DESKTOP-46AJ964>
2024-10-25 11:56:34 +0200

    ajout du fichier 3 + modification du fichier main.h

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - that checks for lowercase character.
+ * @c: the characer to check
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if(c == 'a' || 'z')
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+
+
+}
