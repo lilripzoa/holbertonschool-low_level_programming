db5d482845a338008c3891d37ec7afe069565259
root <root@DESKTOP-46AJ964>
2024-10-25 20:21:47 +0200

    ajout des fichiers 7 et update du main.h

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number.
+ * @n: checks for the last digit
+ * Return: always 0.
+ */
+
+int print_last_digit(int n)
+{
+	int y = n % 10;
+
+	if (y < 0)
+	{
+		return (-y);
+	}
+	else
+	{
+		return (y);
+	}
+}
