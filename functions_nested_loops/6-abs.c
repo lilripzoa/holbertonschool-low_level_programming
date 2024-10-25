4ea049f0b14f18e677b78c768ce764bb92f901f1
root <root@DESKTOP-46AJ964>
2024-10-25 19:35:08 +0200

    update

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -4,21 +4,17 @@
 /**
  * _abs - calcule la valeur absolue d'un entier.
  * @c: l'entier dont on veut la valeur absolue.
- * 
  * Return: la valeur absolue de n.
  */
 
-int _abs(int)
+int _abs(int c)
 {
-	int c;
-	int b = _abs(c)
-	
-	if (b >= '0')
+	if (c < '0')
 	{
-		putchar(c);
+		return (-c);
+	}
+	else
+	{
 		return (c);
 	}
-	return (0);
-
-
 }
