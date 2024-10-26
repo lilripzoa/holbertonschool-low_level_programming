b9c413ea5f9278b98780d8941e674276c30ad74f
root <root@DESKTOP-46AJ964>
2024-10-26 18:31:36 +0200

    ajout des fichiers 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * jack_bauer - that prints every minute of the day.
+ *
+ * Return: always 0.
+ */
+
+void jack_bauer(void)
+{
+	long int minute_dizaine;
+	long int minute_unite;
+	long int heure_dizaine;
+	long int heure_unite;
+
+	for (heure_dizaine = '0'; heure_dizaine <= '2'; heure_dizaine++)
+		for (heure_unite = '0'; heure_unite <= '3'; heure_unite++)
+			for (minute_dizaine = '0'; minute_dizaine <= '5'; minute_dizaine++)					for (minute_unite = '0'; minute_unite <= '9'; minute_unite++)
+			{
+				_putchar(heure_dizaine);
+				_putchar(heure_unite);
+				_putchar('\72');
+				_putchar(minute_dizaine);
+				_putchar(minute_unite);
+				_putchar('\n');
+				sleep(60);
+			}
+
+	_putchar('\n');
+
+
+}
