f3c078154e6286228551819396e51745bdf7985c
root <root@DESKTOP-46AJ964>
2024-10-27 11:55:05 +0100

    ajout des fichiers 9 et update du main.h

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - prints table.
+ * Return: always 0.
+ */
+
+void times_table(void)
+{
+	int a,b,c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+
+			if (c >= 10)
+				_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
+
+			if(b < 9)
+			{
+				_putchar('\54');
+				_putchar('\40');
+			}
+		}
+		_putchar('\n');
+	}
+}
