31ccc5f70547cb226c5031ed0b23ef0533996ad3
root <root@DESKTOP-46AJ964>
2024-10-28 21:49:46 +0100

    update

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -11,12 +11,16 @@ void print_line(int n)
 {
 	int i;
 
-	if (n < 0)
+	if (n > 0)
 	{
-		for (i = 0; i < n; i++)
+		for (i = 0; i <= n; i++)
 		{
 			_putchar('_');
 		}
+	}
+	else
+	{
 		_putchar('\n');
 	}
+	_putchar('\n');
 }
