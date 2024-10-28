c42da7adc4b53a461b7811008ff8c2251f98ec00
root <root@DESKTOP-46AJ964>
2024-10-28 15:57:58 +0100

    ajout des fichiers 2 et update du main.h

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - multiplies two integers..
+ * @a: multiplies
+ * @b: multiplies
+ * Return: the result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
