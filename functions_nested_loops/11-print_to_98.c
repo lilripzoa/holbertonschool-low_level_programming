5c10b98e46fbfc8e07e69308974a0864810568d1
root <root@DESKTOP-46AJ964>
2024-10-28 12:22:16 +0100

    ajout des fichiers 11 et update du main.h

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers.
+ * @n: print from n to 98.
+ * return: 0.
+ */
+
+void print_to_98(int n)
+{
+
+	if (n >= 98)
+	{
+		while (n > 98)
+		{
+			printf("%d", n--);
+			printf("%d", n);
+		}
+	}
+	else
+	{
+		while (n < 98)
+		{
+			printf("%d", n++);
+			printf("%d", n);
+		}
+	}
+}
