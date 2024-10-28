d8b6d1387adfc426e375b9d9cd31d1f54f410e4a
root <root@DESKTOP-46AJ964>
2024-10-28 21:37:55 +0100

    ajout des fichiers 6 et update du main.h

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - that draws a straight line in the terminal.
+ * @n: draws a straight.
+ * Return: \n if n is 0 or less.
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
