465d6566a66ce9cd45ff3875f4d2a1be53c5a1cd
root <root@DESKTOP-46AJ964>
2024-10-28 22:10:21 +0100

    ajout des fichiers 7 et update du main.h

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_diagonal -  draws a diagonal line on the terminal.
+ * @n: draws a diagonal line.
+ * Return: 0.
+ */
+
+void print_diagonal(int n)
+{
+	int a, b;
+
+	if (n > 0)
+	{
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b < a; b++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
