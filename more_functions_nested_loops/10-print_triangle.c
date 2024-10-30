57772bbc1b1b9e2a93efa234787480d178d514e7
root <root@DESKTOP-46AJ964>
2024-10-30 11:05:24 +0100

    ajout des fichiers 10 et update du main.h

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_triangle - prints a triangle
+ * @size: print the triangle.
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+				_putchar(' ');
+			for (j = 1; j <= i; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
+
+
