ef479465a9bc9b312cc131ace5731e01bda20308
root <root@DESKTOP-46AJ964>
2024-10-31 12:27:45 +0100

    exo

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int.
+ * @n: updates the value it points to to 98.
+ * return: 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+/* Main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void reset_to_98(int *n);
