7686d6d27ab75add5f624df29a16919519348bb7
root <root@DESKTOP-46AJ964>
2024-11-01 11:10:17 +0100

    exo

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - that returns the length of a string.
+ * @s: length.
+ * Return: the length of a string.
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
