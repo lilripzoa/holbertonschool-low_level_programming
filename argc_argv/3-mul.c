54ea1d623e9d0ddae84cf6de550f6a43f1f467d6
root <root@DESKTOP-46AJ964>
2024-11-08 13:39:19 +0100

    exo

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include "main.h"
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
  * main - program that multiplies two numbers.
@@ -14,33 +13,16 @@ int main(int argc, char *argv[])
 {
 	int entier1, entier2;
 
-	int i, j;
+	entier1 = entier2 = 0;
 
-	if (argc != 3)
+	if (argc == 3)
 	{
+		entier1 = atoi(argv[1]);
+		entier2 = atoi(argv[2]);
+		printf("%d\n", entier1 * entier2);
+		return (0);
+	}
+	else
 		printf("Error\n");
-		return (1);
-	}
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (!isdigit(argv[i][j]))
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		if (atoi(argv[i]) <= 0)
-		{
-			printf("Error\n");
-			return (1);
-		}
-	}
-	entier1 = atoi(argv[1]);
-	entier2 = atoi(argv[2]);
-
-
-	printf("%d\n", entier1 * entier2);
-	return (0);
+	return (1);
 }
