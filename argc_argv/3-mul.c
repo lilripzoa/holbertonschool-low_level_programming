4b464598724ae01a2d6c59afa8f0a6742d84b5d6
root <root@DESKTOP-46AJ964>
2024-11-08 13:04:40 +0100

    exo

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program that multiplies two numbers.
+ * @argc: argument count.
+ * @argv: argument vector.
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int entier1, entier2;
+
+
+	if (argc != 3)
+	{
+		printf("error 404");
+		return (1);
+	}
+
+	entier1 = atoi(argv[1]);
+	entier2 = atoi(argv[2]);
+
+	if (entier1 == 0 || entier2 == 0)
+	{
+		printf("error 404");
+		return (1);
+	}
+
+	printf("%d\n", entier1 * entier2);
+
+	return (0);
+}
--- argc_argv/main.h
+++ argc_argv/main.h
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 int _putchar(char c);
 int main(int argc, char *argv[]);
