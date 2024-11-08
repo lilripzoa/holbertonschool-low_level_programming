bf99076ff644d1de3d03a8a277421f9c2946bd71
root <root@DESKTOP-46AJ964>
2024-11-08 13:18:08 +0100

    exo

---
+++ argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * main - program that adds positive numbers.
+ * @argc: argument count.
+ * @argv: argument vector.
+ * Return: 0;
+ */
+
+int main(int argc, char *argv[])
+{
+	int a = 0;
+	int i, x;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (x = 0; argv[i][x] != '\0'; x++)
+		{
+			if (!isdigit(argv[i][x]))
+			{
+				printf("error 404\n");
+				return (1);
+			}
+		}
+		a += atoi(argv[i]);
+	}
+	printf("%d\n", a);
+	return (0);
+}
