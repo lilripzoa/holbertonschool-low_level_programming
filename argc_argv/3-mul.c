b94b8217c11a5e9c84e2796b00d3981a4644855d
root <root@DESKTOP-46AJ964>
2024-11-08 13:22:45 +0100

    exo

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,26 +12,17 @@ int main(int argc, char *argv[])
 {
 	int entier1, entier2;
 
+	entier1 = entier2 = 0
 
-	if (argc != 3)
+	if (argc == 3)
+	{
+		entier1 = atoi(argv[1]);
+		entier2 = atoi(argv[2]);
+		printf("%d\n", entier1 * entier2);
+	}
+	else
 	{
 		printf("error 404\n");
 		return (1);
 	}
-
-	entier1 = atoi(argv[1]);
-	entier2 = atoi(argv[2]);
-
-	if (entier1 == 0 || entier2 == 0)
-	{
-		if (argv[1][0] != '0' || argv[2][0] != '0')
-		{
-			printf("error 404\n");
-			return (1);
-		}
-	}
-
-	printf("%d\n", entier1 * entier2);
-
-	return (0);
 }
