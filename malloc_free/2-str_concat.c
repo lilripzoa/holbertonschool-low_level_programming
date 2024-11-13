fe8a1c9229362540d4c884bb807a2c075e57d276
root <root@DESKTOP-46AJ964>
2024-11-13 11:39:47 +0100

    exo

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - function that concatenates two strings.
+ * @s1: string
+ * @s2: string
+ * Return: NULL on failure.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, y;
+	int longueur1 = (s1 == NULL) ? 0 : strlen(s1);
+	int longueur2 = (s2 == NULL) ? 0 : strlen(s2);
+	char *resultat = (char *)malloc((longueur1 + longueur2 + 1) * sizeof(char));
+
+	if (resultat == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < longueur1; i++)
+	{
+		resultat[i] = s1[i];
+	}
+
+	for (y = 0; y < longueur2; y++)
+	{
+		resultat[longueur1 + y] = s2[y];
+	}
+
+	resultat[longueur1 + longueur2] = '\0';
+
+	return (resultat);
+}
