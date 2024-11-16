38d09f58f9d002772c4b94f046792d70e61c2c60
root <root@DESKTOP-46AJ964>
2024-11-16 13:06:37 +0100

    exo

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,40 +13,37 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int a, b;
-	dog_t *toutou = NULL;
+	unsigned int nl, ol, i;
+	dog_t *dog;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
-
-	a = strlen(name) + 1;
-	b = strlen(owner) + 1;
-
-	toutou = malloc(sizeof(dog_t));
-
-	if (toutou == NULL)
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
 		return (NULL);
-
-	toutou->name = malloc(a * sizeof(char));
-
-	if (toutou->name == NULL)
+	for (nl = 0; name[nl]; nl++)
+		;
+	nl++;
+	dog->name = malloc(nl * sizeof(char));
+	if (dog->name == NULL)
 	{
-		free(toutou);
+		free(dog);
 		return (NULL);
 	}
-
-	strcpy(toutou->name, name);
-	toutou->owner = malloc(b * sizeof(char));
-
-	if (toutou->owner == NULL)
+	for (i = 0; i < nl; i++)
+		dog->name[i] = name[i];
+	dog->age = age;
+	for (ol = 0; owner[ol]; ol++)
+		;
+	ol++;
+	dog->owner = malloc(ol * sizeof(char));
+	if (dog->owner == NULL)
 	{
-		free(toutou->name);
-		free(toutou);
+		free(dog->name);
+		free(dog);
 		return (NULL);
 	}
-
-	strcpy(toutou->owner, owner);
-	toutou->age = age;
-
-	return (toutou);
+	for (i = 0; i < ol; i++)
+		dog->owner[i] = owner[i];
+	return (dog);
 }
