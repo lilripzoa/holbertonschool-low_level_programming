da7c25139312902e96d8dde6f7f013afee2499ba
root <root@DESKTOP-46AJ964>
2024-11-16 13:11:54 +0100

    exo

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - function that frees dogs.
+ * @d: pointer.
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name != NULL)
+		free(d->name);
+
+	if (d->owner != NULL)
+		free(d->owner);
+
+	free(d);
+}
