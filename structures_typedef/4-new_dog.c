3aab830a1089af9eea8808c221b97e7ff945189d
root <root@DESKTOP-46AJ964>
2024-11-16 12:47:05 +0100

    exo

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,54 +3,36 @@
 #include <stdlib.h>
 #include <string.h>
 
-/** new_dog - initialize variable.
+/**
+ * new_dog - initialize variable.
  * @name: dog name.
  * @age: dog age.
  * @owner: dog owner.
+ * Return: pointer or null if function fails.
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	char *name_copy, *owner_copy;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
-	if (name != NULL)
-	{
-		name_copy = malloc(strlen(name) + 1);
-		if (name_copy == NULL)
-		{
-			free(new_dog);
-			return (NULL);
-		}
-		strcpy(name_copy, name);
-	}
-	else
-	{
-		name_copy = NULL;
-	}
 
-	if (owner != NULL)
-	{
-		owner_copy = malloc(strlen(owner) + 1);
-		if (owner_copy == NULL)
-		{
-			free(name_copy);
-			free(new_dog);
-			return (NULL);
-		}
-		strcpy(owner_copy, owner);
-	}
-	else
-	{
-		owner_copy = NULL;
-	}
-	new_dog->name = name_copy;
+	new_dog->name = (name != NULL) ? strdup(name) : NULL;
+
+	new_dog->owner = (owner != NULL) ? strdup(owner) : NULL;
+
 	new_dog->age = age;
-	new_dog->owner = owner_copy;
+
+	if ((name != NULL && new_dog->name == NULL) ||
+		(owner != NULL && new_dog->owner == NULL))
+	{
+		free(new_dog->name);
+		free(new_dog->owner);
+		free(new_dog);
+		return (NULL);
+	}
 
 	return (new_dog);
-
 }
