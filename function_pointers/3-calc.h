737f6409f96b95fecc4c9ad76789e991bde1c955
root <root@DESKTOP-46AJ964>
2024-11-20 12:38:54 +0100

    exo

---
+++ function_pointers/3-calc.h
@@ -0,0 +1,64 @@
+#ifndef 3_CALC_H
+#define 3_CALC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+
+/**
+ * op_add - returns the sum of a and b.
+ * @a: sum.
+ * @b: sum.
+ * Return: an integer.
+ */
+
+int op_add(int a, int b);
+
+/**
+ * op_sub - returns the difference of a and b.
+ * @a: integer.
+ * @b: integer.
+ * Return: integer.
+ */
+
+int op_sub(int a, int b);
+
+/**
+ * op_mul - returns the product of a and b.
+ * @a: product.
+ * @b: product.
+ * Return: integer.
+ */
+
+int op_mul(int a, int b);
+
+/**
+ * op_div - returns the result of the division of a by b.
+ * @a: integer
+ * @b: integer
+ * Return: integer.
+ */
+
+int op_div(int a, int b);
+
+/**
+ * op_mod - returns the remainder of the division of a by b.
+ * @a: integer.
+ * @b: integer.
+ * Return: an integer.
+ */
+
+int op_mod(int a, int b);
+
+#endif
---
+++ function_pointers/3-main.c
@@ -0,0 +1,41 @@
+#include "3-calc.h"
+
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on sucess
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, result;
+	int (*operation)(int, int);
+
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	if ((*argv[2] == '/' || *argv[2] == '%') && b == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+
+	result = operation(a, b);
+	printf("%d\n", result);
+
+	return (0);
+}
