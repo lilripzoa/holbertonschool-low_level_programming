a2f1369c4169ac463a5b1bf6490a0b3de0185660
root <root@DESKTOP-46AJ964>
2024-11-21 15:08:15 +0100

    exo

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 /**
  * struct op - Struct op
@@ -16,48 +17,13 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
-/**
- * op_add - returns the sum of a and b.
- * @a: sum.
- * @b: sum.
- * Return: an integer.
- */
-
 int op_add(int a, int b);
-
-/**
- * op_sub - returns the difference of a and b.
- * @a: integer.
- * @b: integer.
- * Return: integer.
- */
 
 int op_sub(int a, int b);
 
-/**
- * op_mul - returns the product of a and b.
- * @a: product.
- * @b: product.
- * Return: integer.
- */
-
 int op_mul(int a, int b);
 
-/**
- * op_div - returns the result of the division of a by b.
- * @a: integer
- * @b: integer
- * Return: integer.
- */
-
 int op_div(int a, int b);
-
-/**
- * op_mod - returns the remainder of the division of a by b.
- * @a: integer.
- * @b: integer.
- * Return: an integer.
- */
 
 int op_mod(int a, int b);
 
