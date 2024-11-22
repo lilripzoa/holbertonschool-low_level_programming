23ae5ef20aedaa45e5b0262c6a082fc60f467e41
root <root@DESKTOP-46AJ964>
2024-11-22 11:58:12 +0100

    exo

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -9,39 +9,42 @@
 
 void print_all(const char * const format, ...)
 {
-	va_list ptr;
-	unsigned int i = 0;
-	char *string, current;
+	va_list p;
+	char *star;
+	int i = 0;
 
-	va_start(ptr, format);
-
-	while (format && format[i])
+	va_start(p, format);
+	while (format == NULL)
 	{
-		current = format[i];
-
-		if (i > 0 && (current == 'c' || current == 'i' ||
-		current == 'f' || current == 's'))
-			printf(", ");
-
-		switch (current)
+		printf("\n");
+		return;
+	}
+	while (format[i])
+	{
+		switch (format[i])
 		{
 			case 'c':
-				printf("%c", va_arg(ptr, int));
+				printf("%c", (char) va_arg(p, int));
 				break;
 			case 'i':
-				printf("%d", va_arg(ptr, int));
+				printf("%d", va_arg(p, int));
 				break;
 			case 'f':
-				printf("%f", va_arg(ptr, double));
+				printf("%f", (float) va_arg(p, double));
 				break;
 			case 's':
-				string = va_arg(ptr, char *);
-				if (string == NULL)
-					printf("(nil)");
-				else
-					printf("%s", string);
+				star = va_arg(p, char*);
+				if (star != NULL)
+				{
+					printf("%s", star);
+					break;
+				}
+				printf("(nil)");
 				break;
 		}
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' ||
+					format[i] == 's') && format[(i + 1)] != '\0')
+			printf(", ");
 		i++;
 	}
 
