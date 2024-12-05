278bc06b23470c2e9d3600a887a22ed597a58d89
root <root@DESKTOP-46AJ964>
2024-12-05 13:58:15 +0100

    modification des fichiers 3-add_node_end et 2-add_node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,28 +12,34 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	int len = 0;
+	int i = 0;
 
-	if (str == NULL)
-	{
-		return (NULL);
-	}
+	while (str[len] != '\0')
+		len++;
 
 	new_node = malloc(sizeof(list_t));
-
 	if (new_node == NULL)
-	{
 		return (NULL);
-	}
 
-	new_node->str = strdup(str);
-
+	new_node->str = malloc(len + 1);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
-	new_node->len = strlen(str);
+
+	while (i < len)
+	{
+		new_node->str[i] = str[i];
+		i++;
+	}
+	new_node->str[i] = '\0';
+
+	new_node->len = len;
 	new_node->next = *head;
+
 	*head = new_node;
-	return (new_node);
+
+	return (*head);
 }
--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -11,44 +11,35 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
+	list_t *current;
 	list_t *new_node;
-	if (str == NULL)
-	{
-		return (NULL);
-	}
+	int i;
+
+	current = *head;
+	while (current && current->next != NULL)
+		current = current->next;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
 
 	new_node = malloc(sizeof(list_t));
-
 	if (new_node == NULL)
-	{
 		return (NULL);
-	}
 
 	new_node->str = strdup(str);
-
 	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
 
-	new_node->len = strlen(str);
+	new_node->len = i;
 	new_node->next = NULL;
 
-	if (*head == NULL)
-	{
-		*head = new_node;
-	}
-	else
-	{
-		list_t *current = *head;
-
-		while (current->next != NULL)
-		{
-			current = current->next;
-		}
+	if (current)
 		current->next = new_node;
-	}
+	else
+		*head = new_node;
 
 	return (new_node);
 }
